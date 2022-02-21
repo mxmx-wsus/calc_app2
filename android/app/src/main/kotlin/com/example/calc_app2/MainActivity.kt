d70e20722a5b26760e8ca035f89684247ff809e5
RyoutaMasuyama <mxmx.khaki@gmail.com>
2022-02-21 22:34:09 +0900

    firstCommit

---
+++ android/app/src/main/kotlin/com/example/calc_app2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calc_app2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,459 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'calc_app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _setNumber = 0;
+
+
+  void _setNum(int num) {
+    if (10000000000 > _setNumber) {
+      setState(() {
+        _setNumber = _setNumber * 10 + num;
+      });
+    }
+  }
+  void _clearNum(){
+    setState(() {
+      _setNumber = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                _setNumber.toString() ,
+                style: TextStyle(
+                  fontSize: 60,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  child: Text(" ",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _clearNum();
+                                  },
+                                  child: Text("CE",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _clearNum();
+                                  },
+                                  child: Text("C",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  child: Text("/",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(7);
+                                  },
+                                  child: Text("7",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(8);
+                                  },
+                                  child: Text("8",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(9);
+                                  },
+                                  child: Text("9",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  child: Text("*",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(4);
+                                  },
+                                  child: Text("4",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(5);
+                                  },
+                                  child: Text("5",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(6);
+                                  },
+                                  child: Text("6",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  child: Text("-",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(1);
+                                  },
+                                  child: Text("1",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(2);
+                                  },
+                                  child: Text("2",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(3);
+                                  },
+                                  child: Text("3",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  child: Text("+",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  child: Text("+/-",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {
+                                    _setNum(0);
+                                  },
+                                  child: Text("0",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  child: Text(".",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  child: Text("=",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
+
