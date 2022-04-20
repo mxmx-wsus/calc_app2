19a5213e5ce5ccbc764b318304c11b6f040391eb
RyoutaMasuyama <mxmx.khaki@gmail.com>
2022-04-20 21:26:12 +0900

    Wip : ボタンごとに押された際の動作を作成

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -1,9 +1,29 @@
-class CalcSystem {
-  var number = [];
-  var op = [];
+const List<String> _checkOperator = ['+','-','×','÷'];
 
-  static void getkey(String letter){
+class CalcSystem {
+  static final List<double> _number = [];
+  static final List<String>  _op = [];
+  static String _numberBuffer = '';
+
+  static void getKey(String letter){
     //TODO ボタンが入力されたらCalcSystem配列に格納するという内容を記載する。
+    if (_checkOperator.contains(letter)){
+      //四則演算子が入力された場合
+      _op.add(letter);
+      _number.add(double.parse(_numberBuffer));
+      _numberBuffer = '';
+    }
+    else if (letter == 'C'){
+      _op.clear();
+      _number.clear();
+      _numberBuffer = '';
+    }
+    else  if (letter == '='){
+      //TODO 計算する
+    }
+    else {
+      _numberBuffer += letter;
+    }
   }
 }
 
